a9300a0b263140cb8a63a64a97680d1261b29648
Ian Christiansen <ianc.songdog@gmail.com>
2022-02-16 12:30:03 -0800

    edited entry message for proper grammar when 0 entries are left

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,9 @@ import { useEntries } from '../../context/PlannerContext';
 export default function Header() {
   const { entries } = useEntries();
   const message =
-    entries.length > 1
-      ? `${entries.length} items left`
-      : `${entries.length} item left`;
+    entries.length === 1
+      ? `${entries.length} item left`
+      : `${entries.length} items left`;
   return (
     <header className={styles.header}>
       <h2>My Planner</h2>
