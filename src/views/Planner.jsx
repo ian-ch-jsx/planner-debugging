a4d628eb0740f28df20c7687cea443b383b49df4
Ian Christiansen <ianc.songdog@gmail.com>
2022-02-16 11:50:08 -0800

    adding an entry functioning

--- src/views/Planner.jsx
+++ src/views/Planner.jsx
@@ -1,6 +1,5 @@
 import AddEntry from '../components/Planner/AddEntry';
 import Entries from '../components/Planner/EntryList';
-import { PlannerProvider } from '../context/PlannerContext';
 import styles from './Planner.css';
 
 export default function Planner() {
